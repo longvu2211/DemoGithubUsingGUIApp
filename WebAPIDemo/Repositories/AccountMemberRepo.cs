a28d49108e27ab2b66706a552b07d3b57159a1d5
Long Vu <vulongbinhduong@gmail.com>
2023-09-14 11:14:25 +0700

    Feature/demo branch vulong (#2)
    * change in MappingProfile.cs
    
    * add ArtTattooService
    
    including Repositories, Controllers

--- WebAPIDemo/Repositories/AccountMemberRepo.cs
+++ WebAPIDemo/Repositories/AccountMemberRepo.cs
@@ -14,6 +14,12 @@ namespace WebAPIDemo.Repositories
             this._context = context;
         }
 
+        public bool CreateAccountMember(AccountMember accountMember)
+        {
+            _context.Add(accountMember);
+            return Save();
+        }
+
         public async Task<AccountMember> GetAccountMember(int id)
         {
             return await _context.AccountMembers.FirstOrDefaultAsync(acc => acc.AccountId == id);
@@ -22,6 +28,12 @@ namespace WebAPIDemo.Repositories
         public async Task<ICollection<AccountMember>> GetAccountMembers()
         {
             return await _context.AccountMembers.OrderBy(acc => acc.FullName).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved >= 0 ? true : false;
         }
     }
 }
